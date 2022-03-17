63acd42d79c47c9e6d60e8b43e8c40b3589e21f5
Aleksandra <a.haniok@gmail.com>
2022-03-17 14:31:48 +0000

    Create the view code and add html files

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+
+# Create the view code
+class PostList(generic.ListView):
+    model = Post
+    # status 1 for publish or 0 for a draft
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6  # limit number of posts that can appear on the front page
