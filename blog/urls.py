21910fc6f5ca8310f53c957d035c6e4c5d219fa5
Aleksandra <a.haniok@gmail.com>
2022-03-17 16:39:43 +0000

    Build the post detail template and wire up the URL of our post detail view

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from . import views
 from django.urls import path
 
-
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home')
+    path('', views.PostList.as_view(), name='home'),
+    # slug keyword name matches the 'slug' parameter in the get method
+    # of the PostDetail class in the blog/views.py file.
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
