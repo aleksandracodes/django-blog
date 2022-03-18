0144a4af203b8b6b46e0596dd675153386530411
Aleksandra <a.haniok@gmail.com>
2022-03-18 08:44:32 +0000

    Get the POST method working for our comments

--- blog/admin.py
+++ blog/admin.py
@@ -5,21 +5,21 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-    
-    prepopulated_fields = {'slug': ('title',)}
-    list_filter = ('status', 'created_on')
+
     list_display = ('title', 'slug', 'status', 'created_on')
-    search_fields = ['title','content']
-    summernote_fields = ('content')  # use Summernotes for blog content (text field)
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-
+    
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
     search_fields = ('name', 'email', 'body')
     actions = ['approve_comments']
 
     def approve_comments(self, request, queryset):
-        query.update(approved=True)
+        queryset.update(approved=True)
